706d4fbf9bc536d0b9cfae6d9efd5fcad9288ff8
zhigang.li <1176838775@qq.com>
2023-11-15 15:28:26 +0800

    feat: add useDocumentTitle hook

---
+++ src/hooks/useDocumentTitle.ts
@@ -0,0 +1,35 @@
+import { useEffect, useRef } from "react";
+
+export interface Options {
+  restoreOnUnmount?: boolean
+  restoreToDefaultTitle?: boolean
+}
+
+const DEFAULT_OPTIONS: Options = {
+  restoreOnUnmount: true,
+  restoreToDefaultTitle: false
+}
+
+const DEFAULT_TITLE = 'Best Hooks'
+
+export const useDocumentTitle = (newTitle: string, options: Options = DEFAULT_OPTIONS) => {
+  const prevTitleRef = useRef<string>(document?.title);
+
+  useEffect(() => {
+    const originalTitle = prevTitleRef.current;
+
+    if (originalTitle !== newTitle) {
+      document.title = newTitle
+    }
+
+    return () => {
+      if (options.restoreOnUnmount) {
+        if (options.restoreToDefaultTitle) {
+          document.title = DEFAULT_TITLE
+        } else {
+          document.title = originalTitle
+        }
+      }
+    }
+  }, [newTitle, options])
+}
